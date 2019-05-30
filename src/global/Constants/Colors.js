7cef8e70e5185337aaff74fa6b4ab54ea7e218e7
RonaldDaniel <ronald.dan.work@gmail.com>
2019-05-30 05:57:30 -0300

    fix pixel 1

--- src/global/Constants/Colors.js
+++ src/global/Constants/Colors.js
@@ -2,5 +2,6 @@ export default {
     backgroundColor : '#F9F9F9',
     FontBlack       : '#24272C',
     ACTIVE          : '#3ec97c',
-    PASSIVE         : 'rgba(36,39,44,0.5)'
+    PASSIVE         : 'rgba(36,39,44,0.5)',
+    FONTGRAY        : 'rgba(36,39,44,0.65)',
 }
--- src/screens/BookingScreen.js
+++ src/screens/BookingScreen.js
@@ -7,11 +7,16 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions,
 } from 'react-native';
 
 import glamorous from 'glamorous-native';
-import Constants  from '../global/Constants'
-
+import Constants  from '../global/Constants';
+const {width,height} = Dimensions.get('window');
+const win = Dimensions.get('window');
+const mainpadding = width*0.5;
+const ratio = win.width / 400;
+const imageRatio = 0.19;
 const Container = glamorous(View)({
   flex: 1,
   width: '100%',
@@ -21,7 +26,7 @@ const Container = glamorous(View)({
   backgroundColor: Constants.Colors.backgroundColor
 })
 const TopView = glamorous(View)({
-  paddingTop: 70,
+  paddingTop: 50,
   width: '100%',
   backgroundColor: 'white'
 })
@@ -29,17 +34,18 @@ const Title = glamorous(Text)({
   color: Constants.Colors.FontBlack,
   fontSize: 24,
   paddingLeft: 20,
+  fontWeight: '400',
 })
 const TopTab = glamorous(View) ({
   flexDirection: 'row',
   width: '100%',
-  marginTop: 50,
+  marginTop: 35,
 })
 const TabButton = glamorous(TouchableOpacity) (({active} = this.props) => ({
   width: '50%',
   flexDirection: 'column',
   alignItems: 'center',
-  paddingBottom: 15,
+  paddingBottom: 18,
   borderBottomWidth: active ? 3 : 0,
   borderColor: Constants.Colors.ACTIVE,
 }))
@@ -56,69 +62,80 @@ const BottomView = glamorous(View)({
   flex: 1,
   width: '100%',
   flexDirection: 'row',
+  paddingHorizontal: 20,
   alignItems: 'flex-start',
   justifyContent: 'center',
 })
 const BookingItem  = glamorous(View) ({
   flexDirection: 'column',
-  paddingVertical: 10,
-  paddingHorizontal: 10,
-  width: '90%',
+  width: '100%',
   borderRadius: 5,
   backgroundColor: 'white',
   shadowColor: 'black',
-  shadowOffset: { width: 0, height: 3 },
-  shadowOpacity: 0.2,
-  shadowRadius: 3,
-  elevation: 3,
-  marginTop: 20,
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.5,
+  shadowRadius: 4,
+  elevation: 5,
+  marginTop: 16,
+  paddingVertical: 16,
   justifyContent: 'center',
   alignItems: 'flex-start',
+})
+const BookingTitleRow = glamorous(View) ({
+  flexDirection: 'row',
+  paddingHorizontal: 20,
+  width: '100%',
+  paddingBottom: 10,
 })
 const DesRow = glamorous(View) ({
   flexDirection: 'row',
   width: '60%',
-  alignItems: 'center',
+  paddingLeft: 20,
+  alignItems: 'flex-end',
   justifyContent: 'space-between',
-  paddingVertical: 15,
+  marginBottom: 19
 })
 const DesMidOval = glamorous(View) ({
   width: 5,
   height: 5,
   borderRadius: 3,
-  backgroundColor: Constants.Colors.PASSIVE
+  marginBottom: 2,
+  backgroundColor: '#DBDBDB'
 })
 const ImageRow = glamorous(View) ({
   flexDirection: 'row',
   width: '100%',
-  marginBottom: 20,
+  marginBottom: 16,
+  paddingHorizontal: 20,
   justifyContent: 'space-between'
 })
 const BookingLine = glamorous(View) ({
   width: '100%',
   height: 1,
-  backgroundColor: Constants.Colors.PASSIVE
+  backgroundColor: '#ECECEC'
 })
 const ImageItem = glamorous(View) ({
-  width: 69,
-  height: 46,
+  width: win.width * imageRatio,
+  height: 267*ratio*imageRatio,
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center'
 })
 const ImageMore = glamorous(View)({
   flexDirection: 'column',
-  width: 69,
-  height: 46,
+  width: win.width * imageRatio,
+  height: 267*ratio*imageRatio,
   position: 'absolute',
+  borderRadius: 4,
   alignItems: 'center',
   justifyContent: 'center',
   backgroundColor: 'rgba(0,0,0,0.5)'
 })
 const CompanyRow = glamorous(View) ({
   flexDirection: 'row',
-  marginTop: 20,
+  marginTop: 16,
   alignItems: 'center',
+  paddingHorizontal: 20,
   justifyContent: 'flex-start'
 })
 const CompanyTitleCol = glamorous(View) ({
@@ -127,34 +144,41 @@ const CompanyTitleCol = glamorous(View) ({
   alignItems: 'flex-start',
   justifyContent: 'space-between',
 })
-const PayDesRow = glamorous(View) ({
+const PayDesRow = glamorous(View) (({bottom} = this.props) => ({
   flexDirection: 'row',
   width: '100%',
-  marginTop: 20,
-  alignItems: 'center',
+  marginTop: 16,
+  paddingHorizontal: 20,
+  paddingBottom: 0 || 5,
+  alignItems: 'flex-end',
   justifyContent: 'space-between',
-})
+}))
 const WashDate = glamorous(View) ({
   flexDirection: 'column',
   paddingRight: 20,
+  height: 38,
   justifyContent: 'space-between',
   alignItems: 'flex-start',
-  height: 40,
 })
 const PaymentPaid = glamorous(View) ({
   flexDirection: 'column',
-  height: 40,
-  paddingRight: 20,
+  height: 38,
+  paddingRight: 10,
   justifyContent: 'space-between',
   alignItems: 'flex-start',
 })
+const ButtonRow = glamorous(View) ({
+  flexDirection: 'row',
+  width: '100%',
+  paddingHorizontal: 20,
+})
 const ViewBookingButton  =  glamorous(TouchableOpacity) ({
   width: '100%',
-  marginTop: 20,
+  marginTop: 15,
   alignItems: 'center',
   justifyContent: 'center',
+  height: 40,
   backgroundColor: Constants.Colors.ACTIVE,
-  paddingVertical: 15,
   borderRadius: 5,
 })
 export default class BookingScreen extends React.Component {
@@ -181,19 +205,39 @@ export default class BookingScreen extends React.Component {
           this.state.upComing ? 
           <BottomView>
             <BookingItem>
-              <CustomText color={Constants.Colors.FontBlack} size = {16}>Vauxhall Corsa</CustomText>
+              <BookingTitleRow>
+                <CustomText color={Constants.Colors.FontBlack} size = {16}>Vauxhall Corsa</CustomText>
+              </BookingTitleRow>
               <DesRow>
-                <CustomText size = {12} color = {Constants.Colors.PASSIVE}>CV62 BNB</CustomText>
+                <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>CV62 BNB</CustomText>
                 <DesMidOval />
-                <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Newcastle NE1</CustomText>
+                <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Newcastle NE1</CustomText>
               </DesRow>
               <ImageRow>
-                <Image source = {Constants.Images.BOOKINGCAR1} style={{resizeMode: 'stretch',width: 69,height: 46}} />
-                <Image source = {Constants.Images.BOOKINGCAR2} style={{resizeMode: 'stretch',width: 69,height: 46}} />
-                <Image source = {Constants.Images.BOOKINGCAR3} style={{resizeMode: 'stretch',width: 69,height: 46}} />
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR1} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR2} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR3} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
                 <TouchableOpacity>
                   <ImageItem>
-                    <Image source = {Constants.Images.BOOKINGCAR1} style={{resizeMode: 'stretch',width: 69,height: 46}} />
+                    <Image 
+                      source = {Constants.Images.BOOKINGCAR1} 
+                      style={styles.image} 
+                      resizeMode = 'contain'/>
                     <ImageMore>
                       <CustomText size = {12} color = {'white'}>+2 more</CustomText>
                     </ImageMore>
@@ -202,43 +246,65 @@ export default class BookingScreen extends React.Component {
               </ImageRow>
               <BookingLine />
               <CompanyRow>
-                <Image source = {Constants.Images.TIMEWASHLOGO} style={{resizeMode: 'stretch',width: 50,height: 33,borderWidth: 1,borderColor: Constants.Colors.PASSIVE,marginRight: 5,}} />
+                <Image source = {Constants.Images.TIMEWASHLOGO} style={{resizeMode: 'stretch',width: 50,height: 33,borderWidth: 1,borderColor: '#ECECEC',marginRight: 8,}} />
                 <CompanyTitleCol>
                   <CustomText color = {Constants.Colors.FontBlack} size = {13}>OXO Care</CustomText>
-                  <CustomText color = {Constants.Colors.PASSIVE} size = {11}>Newcastle NE1</CustomText>
+                  <CustomText color = {Constants.Colors.FONTGRAY} size = {11}>Newcastle NE1</CustomText>
                 </CompanyTitleCol>
               </CompanyRow>
               <PayDesRow>
                 <WashDate>
-                  <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Wash Date -</CustomText>
+                  <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Wash Date -</CustomText>
                   <CustomText size = {12} color = {Constants.Colors.ACTIVE}>21 May 2019, 10:00AM</CustomText>
                 </WashDate>
                 <PaymentPaid>
-                  <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Payment paid -</CustomText>
+                  <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Payment paid -</CustomText>
                   <CustomText size = {12} color = {Constants.Colors.ACTIVE}>£9.25</CustomText>
                 </PaymentPaid>
               </PayDesRow>
-              <ViewBookingButton>
-                <CustomText size = {13} color = {'white'}>View Booking</CustomText>
-              </ViewBookingButton>
+              <ButtonRow>
+                <ViewBookingButton>
+                  <CustomText size = {13} color = {'white'}>View Booking</CustomText>
+                </ViewBookingButton>
+              </ButtonRow>
             </BookingItem>
           </BottomView>
           :
           <BottomView>
             <BookingItem>
-              <CustomText color={Constants.Colors.FontBlack} size = {16}>Vauxhall Corsa</CustomText>
+              <BookingTitleRow>
+                <CustomText color={Constants.Colors.FontBlack} size = {16}>Vauxhall Corsa</CustomText>
+              </BookingTitleRow>
               <DesRow>
-                <CustomText size = {12} color = {Constants.Colors.PASSIVE}>CV62 BNB</CustomText>
+                <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>CV62 BNB</CustomText>
                 <DesMidOval />
-                <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Newcastle NE1</CustomText>
+                <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Newcastle NE1</CustomText>
               </DesRow>
               <ImageRow>
-                <Image source = {Constants.Images.BOOKINGCAR1} style={{resizeMode: 'stretch',width: 69,height: 46}} />
-                <Image source = {Constants.Images.BOOKINGCAR2} style={{resizeMode: 'stretch',width: 69,height: 46}} />
-                <Image source = {Constants.Images.BOOKINGCAR3} style={{resizeMode: 'stretch',width: 69,height: 46}} />
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR1} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR2} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
+                <ImageItem>
+                  <Image 
+                    source = {Constants.Images.BOOKINGCAR3} 
+                    style={styles.image} 
+                    resizeMode = 'contain'/>
+                </ImageItem>
                 <TouchableOpacity>
                   <ImageItem>
-                    <Image source = {Constants.Images.BOOKINGCAR1} style={{resizeMode: 'stretch',width: 69,height: 46}} />
+                    <Image 
+                      source = {Constants.Images.BOOKINGCAR1} 
+                      style={styles.image} 
+                      resizeMode = 'contain'/>
                     <ImageMore>
                       <CustomText size = {12} color = {'white'}>+2 more</CustomText>
                     </ImageMore>
@@ -247,19 +313,19 @@ export default class BookingScreen extends React.Component {
               </ImageRow>
               <BookingLine />
               <CompanyRow>
-                <Image source = {Constants.Images.TIMEWASHLOGO} style={{resizeMode: 'stretch',width: 50,height: 33,borderWidth: 1,borderColor: Constants.Colors.PASSIVE,marginRight: 5,}} />
+                <Image source = {Constants.Images.TIMEWASHLOGO} style={{resizeMode: 'stretch',width: 50,height: 33,borderWidth: 1,borderColor: '#ECECEC',marginRight: 8,}} />
                 <CompanyTitleCol>
                   <CustomText color = {Constants.Colors.FontBlack} size = {13}>OXO Care</CustomText>
-                  <CustomText color = {Constants.Colors.PASSIVE} size = {11}>Newcastle NE1</CustomText>
+                  <CustomText color = {Constants.Colors.FONTGRAY} size = {11}>Newcastle NE1</CustomText>
                 </CompanyTitleCol>
               </CompanyRow>
-              <PayDesRow>
+              <PayDesRow bottom = {5}>
                 <WashDate>
-                  <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Wash Date -</CustomText>
+                  <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Wash Date -</CustomText>
                   <CustomText size = {12} color = {Constants.Colors.FontBlack}>21 May 2019, 10:00AM</CustomText>
                 </WashDate>
                 <PaymentPaid>
-                  <CustomText size = {12} color = {Constants.Colors.PASSIVE}>Payment paid -</CustomText>
+                  <CustomText size = {12} color = {Constants.Colors.FONTGRAY}>Payment paid -</CustomText>
                   <CustomText size = {12} color = {Constants.Colors.FontBlack}>£9.25</CustomText>
                 </PaymentPaid>
               </PayDesRow>
@@ -270,3 +336,10 @@ export default class BookingScreen extends React.Component {
     );
   }
 }
+
+const styles =  StyleSheet.create({
+  image: {
+    width: win.width * imageRatio,
+    height: 267*ratio*imageRatio,
+  }
+})
